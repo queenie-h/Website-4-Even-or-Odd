2d201ddbf937705436c550a5799864e80bd7f3b2
queenieh <queeniehornsby@gmail.com>
2024-11-03 18:35:24 +0000

    first commit

---
+++ script.js
@@ -0,0 +1,12 @@
+
+        
+        prompt("Enter an integer: ");
+        
+        // Check if the number is even or odd
+        if (number % 2 == 0) {
+            System.out.println("The number is Even.");
+        } else {
+            System.out.println("The number is Odd.");
+        }
+        
+        scanner.close();
